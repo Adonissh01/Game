6dcce1d89cde38c9aae60379a01fbe9aaf04db47
Adonissh01 <123102373+Adonissh01@users.noreply.github.com>
2023-02-15 18:46:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,166 +1,138 @@
-const end = document.getElementById("endgame");
-const MprogressBar = document.getElementById("monster-health");
-const Mcolor = document.getElementById("Mcolor");
-const YprogressBar = document.getElementById("your-health");
-const Ycolor = document.getElementById("Ycolor");
-
-
-const attack = document.getElementById("attack");
-const message = document.getElementById("message");
-const newgame = document.getElementById("newgame");
-const buttons = document.getElementById("buttons");
+const monsterh1 = document.getElementById("monsterh1");
+const yourh1 = document.getElementById("yourh1");
+const heal = document.getElementById("heal");
+const end = document.getElementById("give-up");
+const startNewGame = document.getElementById("startNewGame");
 const results = document.getElementById("results");
+const message = document.getElementById("message");
+const attack1 = document.getElementById("attack1");
 
 
-end.addEventListener("click", function (event) {
-
-    event.preventDefault();
-    document.querySelector(".buttons").style.display = "none";
-    document.getElementById("message").innerHTML = "MONSTER WINS!";
-    newgame.style.display = "inline";
-    newgame.style.visibility = "visible";
-
-});
 function subtract(a, b) {
-    let difference = a - b;
-    return Math.floor(difference);
+    
+    return Math.floor(a- b);
 }
 
+//giveup
+ end.addEventListener("click", function (){
 
-attack.addEventListener("click", function (event) {
-    event.preventDefault();
-    let random1 = Math.random() * 100;
-    MprogressBar.value -= random1;
-    let random2 = Math.random() * 100;
-    YprogressBar.value -= random2;
-
-    document.getElementById("results").innerHTML += "<p><span id='monsteee'>Monster</span> attacks and deals " + subtract(100, random1) + "<p><span id='playeee'>Player</span> attacks and deals " + subtract(100, random2);
-
-
-    if (MprogressBar.value === 0) {
-
-        document.querySelector(".buttons").style.display = "none";
-        document.getElementById("message").innerHTML = "YOU WIN!";
-        newgame.style.display = "inline";
-        newgame.style.visibility = "visible";
-
-    }
-    else if (YprogressBar.value === 0) {
-
-        document.querySelector(".buttons").style.display = "none";
-        document.getElementById("message").innerHTML = "YOU LOSE!";
-        newgame.style.display = "inline";
-        newgame.style.visibility = "visible";
-    }
-    else if (YprogressBar.value === 0 && MprogressBar.value === 0) {
-        document.querySelector(".buttons").style.display = "none";
-        document.getElementById("message").innerHTML = "ITS A DRAW!";
-        newgame.style.display = "inline";
-        newgame.style.visibility = "visible";
-    }
-
+   
+     document.querySelector(".buttons").style.display = "none";
+     document.getElementById("results").innerHTML = "MONSTER WINS!";
+     startNewGame.style.display = "inline";
+   startNewGame.style.visibility = "visible";
 });
 
-const heal = document.getElementById("heal");
-let clickcount = 0;
+
+
+
+
+let count = 0;
 let maxvalue = 100;
 
-heal.addEventListener("click", function (event) {
-    event.preventDefault();
-    clickcount++;
+heal.addEventListener("click", function () {
+    
+    count++;
     let random3 = Math.random() * 100;
 
-    if (YprogressBar.value < maxvalue && clickcount < 3) {
-        YprogressBar.value += random3;
-        document.getElementById("results").innerHTML += "<p><span id='playeee'>Player</span> is healed by " + Math.floor(random3);
-    } else if (YprogressBar.value = maxvalue) {
-        document.getElementById("results").innerHTML += "<p><span id='playeee'>Player</span> Health is full ";
-        if (clickcount >= 3) {
+    if (yourh1.value < maxvalue && count < 3) {
+        yourh1.value += random3;
+        document.getElementById("results").innerHTML += "<p> Player is healed by " + Math.floor(random3);
+    } else if (yourh1.value = maxvalue) {
+        document.getElementById("results").innerHTML += " <p>You cannot use heal now";
+        if (count >= 3) {
 
             heal.style.display = "none";
         }
 
     }
+
 });
 
+attack1.addEventListener("click", function () {
+    
+    let random1 = Math.random() * 100;
+    monsterh1.value -= random1;
+    let random2 = Math.random() * 100;
+    yourh1.value -= random2;
+
+    document.getElementById("results").innerHTML += "<p>Monster attacks and deals " + subtract(100, random1) + "<p>Player attacks and deals " + subtract(100, random2);
 
 
-const Sattack = document.getElementById("Sattack");
-const yourhealth = document.getElementById("your-health");
-const monsthealth = document.getElementById("monster-health");
+    if (monsterh1.value === 0) {
 
+        document.querySelector(".buttons").style.display = "none";
+        document.getElementById("message").innerHTML = "YOU WIN!";
+        //start new game
+        
+        startNewGame.style.display = "inline";
+        startNewGame.style.visibility = "visible";
+        
+        
 
-Sattack.addEventListener("click", function (event) {
-    event.preventDefault();
-    let random3 = Math.random() * 100;
-    let random4 = Math.random() * 100;
+    }
+    else if (yourh1.value === 0) {
 
-    while (yourhealth.value < 0.8 * monsthealth.value) {
-        if (random3 < random4) {
-
-            monsthealth.value -= random4;
-            yourhealth.value -= random3;
-
-            document.getElementById("results").innerHTML += "<p><span id='monsteee'>Monster</span> Sattacks and deals " + subtract(100, random3) + "<p><span id='playeee'>Player</span> Sattacks and deals " + subtract(100, random4);
-
-
-            if (MprogressBar.value === 0) {
-
-                document.querySelector(".buttons").style.display = "none";
-                document.getElementById("message").innerHTML = "YOU WIN!";
-                newgame.style.display = "inline";
-                newgame.style.visibility = "visible";
-
-            }
-            else if (YprogressBar.value === 0) {
-
-                document.querySelector(".buttons").style.display = "none";
-                document.getElementById("message").innerHTML = "YOU LOSE!";
-                newgame.style.display = "inline";
-                newgame.style.visibility = "visible";
-            }
-            else if (YprogressBar.value === 0 && MprogressBar.value === 0) {
-                document.querySelector(".buttons").style.display = "none";
-                document.getElementById("message").innerHTML = "ITS A DRAW!";
-                newgame.style.display = "inline";
-                newgame.style.visibility = "visible";
-            }
-        }
-
-
-
-
-
-        else if (random3 > random4) {
-            monsthealth.value -= random3;
-            yourhealth.value -= random4;
-
-            document.getElementById("results").innerHTML += "<p><span id='monsteee'>Monster</span> Sattacks and deals " + subtract(100, random4) + "<p><span id='playeee'>Player</span> Sattacks and deals " + subtract(100, random3);
-
-
-            if (MprogressBar.value === 0) {
-
-                document.querySelector(".buttons").style.display = "none";
-                document.getElementById("message").innerHTML = "YOU WIN!";
-                newgame.style.display = "inline";
-                newgame.style.visibility = "visible";
-
-            }
-            else if (YprogressBar.value === 0) {
-
-                document.querySelector(".buttons").style.display = "none";
-                document.getElementById("message").innerHTML = "YOU LOSE!";
-                newgame.style.display = "inline";
-                newgame.style.visibility = "visible";
-            }
-            else if (YprogressBar.value === 0 && MprogressBar.value === 0) {
-                document.querySelector(".buttons").style.display = "none";
-                document.getElementById("message").innerHTML = "ITS A DRAW!";
-                newgame.style.display = "inline";
-                newgame.style.visibility = "visible";
-            }
-        }
-
+        document.querySelector(".buttons").style.display = "none";
+        document.getElementById("message").innerHTML = "YOU LOSE!";
+        //start new game
+        
+        startNewGame.style.display = "inline";
+        startNewGame.style.visibility = "visible";
+        
+    }
+    else if (yourh1.value === 0 && monsterh1.value === 0) {
+        document.querySelector(".buttons").style.display = "none";
+        document.getElementById("message").innerHTML = "ITS A DRAW!";
+        
+        startNewGame.style.display = "inline";
+        startNewGame.style.visibility = "visible";
+        
     }
 
 });
+startNewGame.addEventListener("click",function(){
+location.reload();
+})
+function specialAttack( ){
+    let ran1 = Math.random() *100;
+    let ran2= Math.random() *100;
+    while(ran1 > ran2){
+        monsterh1.value-=ran1;
+        yourh1.value-=ran2;}
+        document.getElementById("results").innerHTML += "<p>Monster attacks and deals " + subtract(100, random1) + "<p>Player attacks and deals " + subtract(100, random2);
+
+
+    if (monsterh1.value === 0) {
+
+        document.querySelector(".buttons").style.display = "none";
+        document.getElementById("message").innerHTML = "YOU WIN!";
+        //start new game
+        
+        startNewGame.style.display = "inline";
+        startNewGame.style.visibility = "visible";
+        
+        
+
+    }
+    else if (yourh1.value === 0) {
+
+        document.querySelector(".buttons").style.display = "none";
+        document.getElementById("message").innerHTML = "YOU LOSE!";
+        //start new game
+        
+        startNewGame.style.display = "inline";
+        startNewGame.style.visibility = "visible";
+        
+    }
+    else if (yourh1.value === 0 && monsterh1.value === 0) {
+        document.querySelector(".buttons").style.display = "none";
+        document.getElementById("message").innerHTML = "ITS A DRAW!";
+        
+        startNewGame.style.display = "inline";
+        startNewGame.style.visibility = "visible";
+    }
+
+    
+
+}
